670f3460a47aa6a6457307b2a53652db79c38898
fatbo <fatbo@DESKTOP-DS7HSU7>
2022-02-18 16:31:30 -0500

    Add project files.

---
+++ vHex/server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace vHex
+{
+	public class server : BaseScript
+	{
+		[Command("hex")]
+		public void Hex(int source, List<object> args, string raw)
+		{
+			Player player = Players[source];
+			var steam = GetPlayerIdentifier(source.ToString(), 0);
+
+			TriggerClientEvent(player, "chat:addMessage", new
+			{
+				color = new[] { 255, 0, 0 },
+				multiline = true,
+				args = new[] {steam}
+			});
+
+		}
+
+	}
+}
